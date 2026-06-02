40f07bcba8ad7859e0686447a2c88d195d24f443
doradyra5-create <doradyra5@gmail.com>
2026-06-02 23:29:37 +0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-:root {
-    --color-black: #000000;
-    --color-refresh-black: #121212;
-    --color-attractive-black: #222222;
-    --color-white: #ffffff;
-    --color-grey: #D9D9D9;
-    --color-red: #D62020;
-
-}
